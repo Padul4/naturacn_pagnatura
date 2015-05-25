786a94ba3b26cb4ccf71669ee8d07ce4d7fcbad4
Raphael Lopes <ph.campanario07@gmail.com>
2015-05-25 19:29:43 -0300

    Aplicação do modal

--- frontend/app/js/main.js
+++ frontend/app/js/main.js
@@ -5,6 +5,20 @@ import Cards from './cards';
 var Main = {
   init: function () {
     Cards.init();
+    this.bind();
+  },
+  bind: function() {
+    var _that = this;
+    $('.pagnatura-modal-box .btn-close').on('click', function() {
+      $('#pn-modal').fadeOut(function() {
+        $('body').removeClass('modalOpened');
+      });
+    });
+    $('.pagnatura-faixa-wrapper .pagnatura-cloud .btn-link').on('click', function() {
+      $('#pn-modal').fadeIn(function() {
+        $('body').addClass('modalOpened');
+      });
+    });
   }
 };
 $(document).ready(function() {
